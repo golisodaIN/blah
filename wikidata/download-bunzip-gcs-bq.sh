bdb60c1fed5564117fc25863c8544e107d449d66
Felipe Hoffa <felipe.hoffa@gmail.com>
2016-03-10 21:14:39 -0800

    Update download-bunzip-gcs-bq.sh

--- wikidata/download-bunzip-gcs-bq.sh
+++ wikidata/download-bunzip-gcs-bq.sh
@@ -1,5 +1,14 @@
-wget https://dumps.wikimedia.org/wikidatawiki/entities/latest-all.json.bz2; \
-lbunzip2 latest-all.json.bz2; \
-gsutil -o GSUtil:parallel_composite_upload_threshold=150M cp latest-all.json gs://fh-bigquery/wikidata/; \
-bq load --field_delimiter="tab" --max_bad_records 1 --replace wikidata.latest_raw gs://fh-bigquery/wikidata/latest-all.json item
+#! /bin/bash
+apt-get update
+apt-get install -y wget lbzip2
 
+wget https://dumps.wikimedia.org/wikidatawiki/entities/latest-all.json.bz2; date;\
+lbunzip2 latest-all.json.bz2; date;\
+gsutil -o GSUtil:parallel_composite_upload_threshold=150M cp latest-all.json gs://fh-bigquery/wikidata/; date;\
+bq load --field_delimiter="tab" --max_bad_records 1 --replace wikidata.latest_raw gs://fh-bigquery/wikidata/latest-all.json item; date
+
+
+# Download: 4.1 GB, 2 MB/s, 35 minutes
+# lbunzip2: 65 GB, 7 minutes (16 cpus)
+# gsutil cp: 65 GB, 3 minutes (16 cpus)
+# bq: 3 minutes ingest
