150371488a2bc0de5d8ec0ca0d49a86ab0609790
Felipe Hoffa <felipe.hoffa@gmail.com>
2016-03-11 14:33:00 -0800

    Create fictional_and_superheroes.sql

---
+++ wikidata/fictional_and_superheroes.sql
@@ -0,0 +1,19 @@
+SELECT en_wiki, SUM(requests) requests,
+  FIRST(superhero) superhero, FIRST(fictional_character) fictional_character, FIRST(occupation_superhero) occupation_superhero,
+  VARIANCE(LOG(requests)) varlog
+FROM [fh-bigquery:wikipedia.pagecounts_201602_en_top365k] a
+JOIN (
+  SELECT en_wiki, 
+    EVERY(instance_of.numeric_id!=188784) WITHIN RECORD superhero,
+    EVERY(instance_of.numeric_id!=95074) WITHIN RECORD fictional_character,
+    EVERY(occupation.numeric_id!=188784) WITHIN RECORD occupation_superhero,
+  FROM [wikidata.latest_en_v1] 
+  OMIT RECORD IF (EVERY(instance_of.numeric_id!=188784)
+  AND EVERY(instance_of.numeric_id!=95074)
+  AND EVERY(occupation.numeric_id!=188784))
+) b
+ON a.title=b.en_wiki
+GROUP BY 1
+HAVING varlog<0.5
+ORDER BY 2 DESC
+LIMIT 100
