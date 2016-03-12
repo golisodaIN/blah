72044f1b65775b726f8668be188eb37ad7dc4cf7
Felipe Hoffa <felipe.hoffa@gmail.com>
2016-03-11 16:17:35 -0800

    Update fictional_and_superheroes.sql

--- wikidata/fictional_and_superheroes.sql
+++ wikidata/fictional_and_superheroes.sql
@@ -7,7 +7,7 @@ JOIN (
     NOT EVERY(instance_of.numeric_id!=188784) WITHIN RECORD superhero,
     NOT EVERY(instance_of.numeric_id!=95074) WITHIN RECORD fictional_character,
     NOT EVERY(occupation.numeric_id!=188784) WITHIN RECORD occupation_superhero,
-  FROM [wikidata.latest_en_v1] 
+  FROM [fh-bigquery:wikidata.latest_en_v1] 
   OMIT RECORD IF (EVERY(instance_of.numeric_id!=188784)
   AND EVERY(instance_of.numeric_id!=95074)
   AND EVERY(occupation.numeric_id!=188784))
