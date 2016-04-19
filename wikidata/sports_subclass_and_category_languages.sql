0caa001636c518030b9a696a244212663ee7c00f
Felipe Hoffa <felipe.hoffa@gmail.com>
2016-04-19 05:42:16 -0700

    Create sports_subclass_and_category_languages.sql

---
+++ wikidata/sports_subclass_and_category_languages.sql
@@ -0,0 +1,18 @@
+SELECT SUM(requests) reqs, FIRST(en_label), title
+FROM [fh-bigquery:wikipedia.pagecounts_201509] a
+JOIN (
+  SELECT es_wiki, en_label, en_wiki, fr_wiki, ja_wiki
+  FROM FLATTEN([fh-bigquery:wikidata.latest_enesjafrde_v1], subclass_of) a
+  JOIN (
+    SELECT numeric_id
+    FROM [fh-bigquery:wikidata.subclasses] b
+    WHERE subclass_of_numeric_id=349 # sports
+    GROUP BY 1
+  ) b
+  ON a.subclass_of.numeric_id=b.numeric_id
+) b
+ON a.title=b.es_wiki
+WHERE language='es'
+GROUP BY title 
+ORDER BY 1 DESC
+LIMIT 100
