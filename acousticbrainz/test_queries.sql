38ac411b5b9db1eea7bb66b20ff4ce6a93e5974d
Felipe Hoffa <felipe.hoffa@gmail.com>
2016-04-25 10:18:15 -0700

    Update test_queries.sql

--- acousticbrainz/test_queries.sql
+++ acousticbrainz/test_queries.sql
@@ -1,7 +1,19 @@
+Top artists and songs:
+
 SELECT COUNT(*) c,
        JSON_EXTRACT_SCALAR(item, '$.metadata.tags.artist[0]') artist,
        JSON_EXTRACT_SCALAR(item, '$.metadata.tags.title[0]') song,
 FROM [test_acousticbrainz.lowlevel] 
 GROUP BY 2,3
+ORDER BY 1 DESC
+LIMIT 100
+
+
+Top genders:
+
+SELECT COUNT(*) c,
+       JSON_EXTRACT_SCALAR(item, '$.highlevel.gender.value') gender,
+FROM [test_acousticbrainz.highlevel] 
+GROUP BY 2
 ORDER BY 1 DESC
 LIMIT 100
