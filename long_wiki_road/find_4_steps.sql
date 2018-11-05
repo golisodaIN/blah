0af68d30cb743a24e51fa90749e0c91ca83b2496
Felipe Hoffa <fhoffa@users.noreply.github.com>
2018-11-05 13:44:48 -0800

    Create find_4_steps.sql

---
+++ long_wiki_road/find_4_steps.sql
@@ -0,0 +1,11 @@
+SELECT a.a, a.bridge.b, a.c, b.bridge.b b_b, b.c b_c, a.bridge.prob*b.bridge.prob prob
+FROM `fh-bigquery.wikipedia_vt.clicstream_c_comb_clust_a` a
+JOIN `fh-bigquery.wikipedia_vt.clicstream_c_comb_clust_c` b
+ON a.c=b.a
+WHERE a.a='List_of_Presidents_of_the_United_States'
+AND b.c='Lady_Gaga'
+AND a.bridge.b!=b.bridge.b
+AND a.a!=b.bridge.b
+AND a.bridge.b!=b.c
+ORDER BY prob DESC
+LIMIT 30
