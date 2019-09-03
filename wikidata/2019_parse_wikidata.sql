31db26036c1999e87ff31235d2b8b92d3ded54b2
Felipe Hoffa <fhoffa@users.noreply.github.com>
2019-09-03 14:58:23 -0700

    Update 2019_parse_wikidata.sql

--- wikidata/2019_parse_wikidata.sql
+++ wikidata/2019_parse_wikidata.sql
@@ -32,8 +32,10 @@ RETURNS STRUCT <
   ,genre ARRAY<STRUCT<numeric_id INT64>>
   ,industry ARRAY<STRUCT<numeric_id INT64>>
   ,subclass_of ARRAY<STRUCT<numeric_id INT64>>
-  ,coordinate_location ARRAY<STRUCT<latitude FLOAT64, longitude FLOAT64, altitude FLOAT64>>
+  ,coordinate_location ARRAY<STRUCT<latitude FLOAT64, longitude FLOAT64>>
   ,iso_3166_alpha3 ARRAY<STRUCT<value STRING>> 
+  ,member_of ARRAY<STRUCT<value STRING>> 
+  ,from_fictional_universe ARRAY<STRUCT<value STRING>> 
 >
 
 LANGUAGE js AS """
@@ -89,29 +91,16 @@ LANGUAGE js AS """
     for(var i in obj.claims[pnumber]) {
       if (!obj.claims[pnumber][i].mainsnak.datavalue) continue;
       var claim = {}
-      claim['altitude']=obj.claims[pnumber][i].mainsnak.datavalue.value['altitude']
       claim['longitude']=obj.claims[pnumber][i].mainsnak.datavalue.value['longitude']
       claim['latitude']=obj.claims[pnumber][i].mainsnak.datavalue.value['latitude']
       snaks.push(claim) 
     }
     return snaks
   }
+  function snaksNum(obj, pnumber) {
+    return snaksValue(obj, pnumber, 'numeric_id');
+  }
   
-  instance_of=snaks(obj, 'P31', 'numeric_id');
-  gender=snaks(obj, 'P21', 'numeric_id');
-  date_of_birth=snaks(obj, 'P569', 'time');
-  date_of_death=snaks(obj, 'P569', 'time');
-  place_of_birth=snaks(obj, 'P19', 'numeric_id');
-  country_of_citizenship=snaks(obj, 'P27', 'numeric_id');
-  country=snaks(obj, 'P17', 'numeric_id');
-  occupation=snaks(obj, 'P106', 'numeric_id');
-  instrument=snaks(obj, 'P1303', 'numeric_id');
-  genre=snaks(obj, 'P136', 'numeric_id');
-  industry=snaks(obj, 'P452', 'numeric_id');
-  subclass_of=snaks(obj, 'P279', 'numeric_id');
-  coordinate_location=snaksLoc(obj, 'P625');
-  iso_3166_alpha3=snaksValue(obj, 'P298', 'value');
-
   return {
     id: obj.id,
     en_wiki: obj.sitelinks ? (obj.sitelinks.enwiki ? obj.sitelinks.enwiki.encoded : null) : null,
@@ -132,20 +121,22 @@ LANGUAGE js AS """
     descriptions: descriptions,
     sitelinks: sitelinks,
     aliases: aliases,
-    instance_of: instance_of,
-    gender: gender,
-    date_of_birth: date_of_birth,
-    date_of_death: date_of_death,
-    place_of_birth: place_of_birth,
-    country_of_citizenship: country_of_citizenship,
-    country: country,
-    occupation: occupation,
-    instrument: instrument,
-    genre: genre,
-    industry: industry,
-    subclass_of: subclass_of,
-    coordinate_location: coordinate_location,
-    iso_3166_alpha3: iso_3166_alpha3,
+    instance_of: snaksNum(obj, 'P31'),
+    gender: snaksNum(obj, 'P21'),
+    date_of_birth: snaks(obj, 'P569', 'time'),
+    date_of_death: snaks(obj, 'P569', 'time'),
+    place_of_birth: snaksNum(obj, 'P19'),
+    country_of_citizenship: snaksNum(obj, 'P27'),
+    country: snaksNum(obj, 'P17'),
+    occupation: snaksNum(obj, 'P106'),
+    instrument: snaksNum(obj, 'P1303'),
+    genre: snaksNum, 'P136'),
+    industry: snaksNum(obj, 'P452'),
+    subclass_of: snaksNum(obj, 'P279'),
+    coordinate_location: snaksLoc(obj, 'P625'),
+    iso_3166_alpha3: snaksValue(obj, 'P298', 'value'),
+    member_of: snaksNum(obj, 'P463'),
+    from_fictional_universe: snaksNum(obj, 'P1080'),
   }
 
 """;
