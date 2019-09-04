fe4092ba9535a1374124d1c9c1168fbf5c022263
Felipe Hoffa <fhoffa@users.noreply.github.com>
2019-09-03 22:41:27 -0700

    Update 2019_trivia_superheroes.sql

--- wikidata/2019_trivia_superheroes.sql
+++ wikidata/2019_trivia_superheroes.sql
@@ -1,9 +1,12 @@
 SELECT views, title
-   , ARRAY(SELECT DISTINCT AS STRUCT en_label, numeric_id FROM `fh-bigquery.wikidata.subclasses_20190822` a JOIN UNNEST(occupation) b USING(numeric_id)) occupation
-   , ARRAY(SELECT DISTINCT AS STRUCT en_label, numeric_id FROM `fh-bigquery.wikidata.subclasses_20190822` a JOIN UNNEST(instance_of) b USING(numeric_id)) instance_of
+   , ARRAY(SELECT DISTINCT AS STRUCT en_label, numeric_id FROM `fh-bigquery.wikidata.subclasses_20190822` JOIN UNNEST(occupation) USING(numeric_id)) occupation
+   , ARRAY(SELECT DISTINCT AS STRUCT en_label, numeric_id FROM `fh-bigquery.wikidata.subclasses_20190822` JOIN UNNEST(instance_of) USING(numeric_id)) instance_of
+   , ARRAY(SELECT AS STRUCT en_label, numeric_id FROM `fh-bigquery.wikidata.wikidata_latest_20190822` JOIN UNNEST(a.member_of) USING(numeric_id)) member_of
+   , ARRAY(SELECT AS STRUCT en_label, numeric_id FROM `fh-bigquery.wikidata.wikidata_latest_20190822` JOIN UNNEST(a.from_fictional_universe) USING(numeric_id)) from_fictional_universe
 FROM (
   SELECT SUM(views) views, title
     , ANY_VALUE(occupation) occupation, ANY_VALUE(instance_of) instance_of
+    , ANY_VALUE(member_of) member_of, ANY_VALUE(from_fictional_universe) from_fictional_universe
   FROM `fh-bigquery.wikipedia_v3.pageviews_2019` a
   JOIN (
     SELECT *
@@ -23,5 +26,5 @@ FROM (
   AND a.wiki='en'
   AND DATE(a.datehour)='2019-01-08'
   GROUP BY title
-)
+) a
 ORDER BY views DESC
