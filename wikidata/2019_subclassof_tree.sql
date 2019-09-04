f1796a8e1c2f7dc56eb0ac1f55f473a710e35aa7
Felipe Hoffa <fhoffa@users.noreply.github.com>
2019-09-03 23:56:11 -0700

    Update 2019_subclassof_tree.sql

--- wikidata/2019_subclassof_tree.sql
+++ wikidata/2019_subclassof_tree.sql
@@ -3,19 +3,11 @@ AS
 
 SELECT a.*, b.en_label subclass_of_label, 1 level
 FROM (
-  SELECT a.*, b.en_label, c.numeric_id subclass_of_numeric_id, FORMAT('Q%i', c.numeric_id) subclass_of_id
-  FROM (
-    SELECT FORMAT('Q%i', b.numeric_id) id, ANY_VALUE(b.numeric_id) numeric_id, COUNT(DISTINCT id) c
-    FROM `fh-bigquery.wikidata.wikidata_latest_20190822` a, UNNEST(subclass_of)b
-    GROUP BY 1 
-  ) a
-  JOIN `fh-bigquery.wikidata.wikidata_latest_20190822` b
-  USING(numeric_id)
-  , UNNEST(subclass_of) c
+  SELECT a.numeric_id, en_label, b.numeric_id subclass_of_numeric_id
+  FROM `fh-bigquery.wikidata.wikidata_latest_20190822` a, UNNEST(a.subclass_of) b
 ) a
-LEFT JOIN `fh-bigquery.wikidata.wikidata_latest_20190822` b
-ON subclass_of_numeric_id = b.numeric_id
-ORDER BY c DESC
+JOIN `fh-bigquery.wikidata.wikidata_latest_20190822` b
+ON a.subclass_of_numeric_id=b.numeric_id
 ;
 
 LOOP
@@ -28,12 +20,12 @@ LOOP
 
     INSERT INTO `wikidata.subclasses_20190822`
 
-    SELECT a.id, a.numeric_id, a.c, a.en_label, b.subclass_of_numeric_id, b.subclass_of_id, b.subclass_of_label, MIN(b.level+1) level
+    SELECT a.numeric_id, a.en_label, b.subclass_of_numeric_id, b.subclass_of_label, MIN(b.level+1) level
     FROM `wikidata.subclasses_20190822` a
     JOIN `wikidata.subclasses_20190822` b
-    ON a.subclass_of_id = b.id
-    WHERE CONCAT(a.id,b.subclass_of_id) NOT IN (SELECT CONCAT(id,subclass_of_id) FROM `wikidata.subclasses_20190822`)
-    GROUP BY 1,2,3,4,5,6,7
+    ON a.subclass_of_numeric_id = b.numeric_id
+    WHERE STRUCT(a.numeric_id,b.subclass_of_numeric_id) NOT IN (SELECT STRUCT(numeric_id,subclass_of_numeric_id) FROM `wikidata.subclasses_20190822`)
+    GROUP BY 1,2,3,4
     ;
 
     SET row_diff = (SELECT COUNT(*) FROM `wikidata.subclasses_20190822`) - row_count;
