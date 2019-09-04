d1d759759ea0f5ececfcc7ea1a51a27d79ccb729
Felipe Hoffa <fhoffa@users.noreply.github.com>
2019-09-03 21:54:56 -0700

    Update 2019_trivia_superheroes.sql

--- wikidata/2019_trivia_superheroes.sql
+++ wikidata/2019_trivia_superheroes.sql
@@ -1,13 +1,13 @@
 SELECT views, title
-   , ARRAY(SELECT DISTINCT AS STRUCT en_label, numeric_id FROM `fh-bigquery.wikidata.subclasses_20190822_b` a JOIN UNNEST(occupation) b USING(numeric_id)) occupation
-   , ARRAY(SELECT DISTINCT AS STRUCT en_label, numeric_id FROM `fh-bigquery.wikidata.subclasses_20190822_b` a JOIN UNNEST(instance_of) b USING(numeric_id)) instance_of
+   , ARRAY(SELECT DISTINCT AS STRUCT en_label, numeric_id FROM `fh-bigquery.wikidata.subclasses_20190822` a JOIN UNNEST(occupation) b USING(numeric_id)) occupation
+   , ARRAY(SELECT DISTINCT AS STRUCT en_label, numeric_id FROM `fh-bigquery.wikidata.subclasses_20190822` a JOIN UNNEST(instance_of) b USING(numeric_id)) instance_of
 FROM (
   SELECT SUM(views) views, title
     , ANY_VALUE(occupation) occupation, ANY_VALUE(instance_of) instance_of
   FROM `fh-bigquery.wikipedia_v3.pageviews_2019` a
   JOIN (
     SELECT *
-    FROM `fh-bigquery.wikidata.wikidata_latest_20190822_b` 
+    FROM `fh-bigquery.wikidata.wikidata_latest_20190822` 
     WHERE EXISTS (
       SELECT * FROM (
         SELECT * FROM UNNEST(occupation)
