eefb194cc88868fdd54e7e6b97bea5400c12bd94
Felipe Hoffa <fhoffa@users.noreply.github.com>
2019-09-03 23:04:55 -0700

    Create 2019_trivia_top_politicians.sql

---
+++ wikidata/2019_trivia_top_politicians.sql
@@ -0,0 +1,31 @@
+SELECT views, title
+   , ARRAY(SELECT DISTINCT AS STRUCT en_label, numeric_id FROM `fh-bigquery.wikidata.subclasses_20190822` JOIN UNNEST(occupation) USING(numeric_id)) occupation
+   , ARRAY(SELECT DISTINCT AS STRUCT en_label, numeric_id FROM `fh-bigquery.wikidata.subclasses_20190822` JOIN UNNEST(instance_of) USING(numeric_id)) instance_of
+   , ARRAY(SELECT AS STRUCT en_label, numeric_id FROM `fh-bigquery.wikidata.wikidata_latest_20190822` JOIN UNNEST(a.gender) USING(numeric_id)) gender
+   , date_of_birth
+FROM (
+  SELECT SUM(views) views, title
+    , ANY_VALUE(occupation) occupation, ANY_VALUE(instance_of) instance_of
+    , ANY_VALUE(gender) gender, ANY_VALUE(date_of_birth) date_of_birth
+  FROM `fh-bigquery.wikipedia_v3.pageviews_2019` a
+  JOIN (
+    SELECT *
+    FROM `fh-bigquery.wikidata.wikidata_latest_20190822` 
+    WHERE EXISTS (
+      SELECT * FROM (
+        SELECT * FROM UNNEST(occupation)
+        UNION ALL
+        SELECT * FROM UNNEST(instance_of) 
+      )
+      WHERE numeric_id=82955
+      LIMIT 1
+    )
+    AND en_wiki IS NOT null
+    AND ARRAY_LENGTH(sitelinks)>=11
+  ) b
+  ON a.title=b.en_wiki
+  AND a.wiki='en'
+  AND DATE(a.datehour)='2019-08-08'
+  GROUP BY title
+) a
+ORDER BY views DESC
