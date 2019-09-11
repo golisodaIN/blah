17fc09b800ff077d1d92e2e76e56c1a91bd9700d
Felipe Hoffa <fhoffa@users.noreply.github.com>
2019-09-10 21:45:03 -0700

    Create load.sh

---
+++ flights/load.sh
@@ -0,0 +1,7 @@
+for s in `seq 2019 2019`
+do
+        for m in `seq 1 12`
+        do 
+wget https://transtats.bts.gov/PREZIP/On_Time_Reporting_Carrier_On_Time_Performance_1987_present_${s}_${m}.zip
+done
+done
