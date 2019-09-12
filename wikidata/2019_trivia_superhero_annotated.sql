7dd497256c0a5be7ed1caa8d9e35956a3779baa5
Felipe Hoffa <fhoffa@users.noreply.github.com>
2019-09-12 00:14:39 -0700

    Update 2019_trivia_superhero_annotated.sql

--- wikidata/2019_trivia_superhero_annotated.sql
+++ wikidata/2019_trivia_superhero_annotated.sql
@@ -1,8 +1,8 @@
 SELECT views, title
-   , [(SELECT IFNULL(STRING_AGG(DISTINCT en_label),'') FROM `fh-bigquery.wikidata.subclasses_20190822` JOIN UNNEST(occupation) USING(numeric_id)) 
-   , (SELECT STRING_AGG(DISTINCT en_label) FROM `fh-bigquery.wikidata.subclasses_20190822` JOIN UNNEST(instance_of) USING(numeric_id)) 
-   , (SELECT IFNULL(STRING_AGG(en_label),'') FROM `fh-bigquery.wikidata.wikidata_latest_20190822` JOIN UNNEST(a.member_of) USING(numeric_id)) 
-   , (SELECT IFNULL(STRING_AGG(en_label),'') FROM `fh-bigquery.wikidata.wikidata_latest_20190822` JOIN UNNEST(a.from_fictional_universe) USING(numeric_id))] annotated
+   , [(SELECT IFNULL(STRING_AGG(DISTINCT en_label, ', '),'') FROM `fh-bigquery.wikidata.subclasses_20190822` JOIN UNNEST(occupation) USING(numeric_id)) 
+   , (SELECT STRING_AGG(DISTINCT en_label, ', ') FROM `fh-bigquery.wikidata.subclasses_20190822` JOIN UNNEST(instance_of) USING(numeric_id)) 
+   , (SELECT IFNULL(STRING_AGG(en_label, ', '),'') FROM `fh-bigquery.wikidata.wikidata_latest_20190822` JOIN UNNEST(a.member_of) USING(numeric_id)) 
+   , (SELECT IFNULL(STRING_AGG(en_label, ', '),'') FROM `fh-bigquery.wikidata.wikidata_latest_20190822` JOIN UNNEST(a.from_fictional_universe) USING(numeric_id))] annotated
 FROM (
   SELECT SUM(views) views, title
     , ANY_VALUE(occupation) occupation, ANY_VALUE(instance_of) instance_of
