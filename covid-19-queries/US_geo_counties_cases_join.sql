cf7d36b07f0be9c4986cc15b61e754605ae35a5a
Felipe Hoffa <fhoffa@users.noreply.github.com>
2020-04-13 14:16:32 -0700

    Update US_geo_counties_cases_join.sql

--- covid-19-queries/US_geo_counties_cases_join.sql
+++ covid-19-queries/US_geo_counties_cases_join.sql
@@ -5,4 +5,5 @@ SELECT
 FROM `bigquery-public-data.covid19_usafacts.confirmed_cases` a
 JOIN `bigquery-public-data.geo_us_boundaries.counties` b
 ON a.county_fips_code = b.state_fips_code || b.county_fips_code
+# 12.7 sec elapsed, 182.1 MB processed
 # (h/t Amir Hormati)
