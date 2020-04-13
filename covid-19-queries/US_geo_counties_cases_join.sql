4579aa4510dd3ff0b17a12cb7ba0dc428789680b
Felipe Hoffa <fhoffa@users.noreply.github.com>
2020-04-13 14:15:57 -0700

    Update US_geo_counties_cases_join.sql

--- covid-19-queries/US_geo_counties_cases_join.sql
+++ covid-19-queries/US_geo_counties_cases_join.sql
@@ -4,7 +4,5 @@ SELECT
   county_geom AS the_geom
 FROM `bigquery-public-data.covid19_usafacts.confirmed_cases` a
 JOIN `bigquery-public-data.geo_us_boundaries.counties` b
-ON CAST(a.county_fips_code as string) 
-   = CONCAT(b.state_fips_code, b.county_fips_code)
-   
+ON a.county_fips_code = b.state_fips_code || b.county_fips_code
 # (h/t Amir Hormati)
