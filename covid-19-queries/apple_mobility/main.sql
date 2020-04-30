75ce3209d05b2c3b230191561da895af662e3561
Felipe Hoffa <fhoffa@users.noreply.github.com>
2020-04-29 21:38:51 -0700

    Create main.sql

---
+++ covid-19-queries/apple_mobility/main.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE TABLE 
+  `fh-bigquery.public_dump.applemobilitytrends`
+OPTIONS (
+  description="https://www.apple.com/covid19/mobility"
+) AS
+WITH data AS (
+  SELECT geo_type, region, transportation_type, unpivotted.*
+    , LEAST(-1 + value/100, 0.2) percent
+  FROM `fh-bigquery.temp.latestAppleCovidData` a
+    , UNNEST(fhoffa.x.cast_kv_array_to_date_float(
+      fhoffa.x.unpivot(a, '_2020'), '_%Y_%m_%d')) unpivotted
+), annotated_data AS (
+  SELECT *
+    , -1+EXP(AVG(LOG(1+percent)) OVER(PARTITION BY geo_type, region, transportation_type ORDER BY date
+      rows between 6 preceding and current row)) avg7day 
+    , geo_type||transportation_type||region series_id
+  FROM data
+)
+SELECT *, ROW_NUMBER() OVER(ORDER BY current_percent) rank 
+FROM (
+  SELECT *
+    , (SELECT percent 
+       FROM annotated_data 
+       WHERE a.series_id=series_id 
+       AND date=(SELECT MAX(date) FROM annotated_data)
+       ) current_percent
+    , (SELECT MIN(date) FROM annotated_data WHERE a.series_id=series_id AND avg7day<-.25) first_drop_date 
+  FROM annotated_data a
+)
