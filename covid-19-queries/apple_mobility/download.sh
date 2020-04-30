1e3bac379133f44e21a2daf3fdd7cb8d3fb10603
Felipe Hoffa <fhoffa@users.noreply.github.com>
2020-04-29 21:41:15 -0700

    Create download.sh

---
+++ covid-19-queries/apple_mobility/download.sh
@@ -0,0 +1,5 @@
+x=$(curl -s https://covid19-static.cdn-apple.com/covid19-mobility-data/current/v1/index.json)
+url=$( echo https://covid19-static.cdn-apple.com`jq -n "$x" |jq -r '.basePath'``jq -n "$x"|jq -r '.regions."en-us".csvPath'
+`)
+curl -s $url -o /tmp/latestAppleCovidData.csv
+bq load --autodetect --replace fh-bigquery:temp.latestAppleCovidData /tmp/latestAppleCovidData.csv
