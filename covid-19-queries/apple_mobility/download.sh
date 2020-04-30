f43de3e441d1f94513860a0f44f0f7ab464b3425
Felipe Hoffa <fhoffa@users.noreply.github.com>
2020-04-29 21:41:28 -0700

    Update download.sh

--- covid-19-queries/apple_mobility/download.sh
+++ covid-19-queries/apple_mobility/download.sh
@@ -1,4 +1,4 @@
-x=$(curl -s https://covid19-static.cdn-apple.com/covid19-mobility-data/current/v1/index.json)
+x=$(curl -s https://covid19-static.cdn-apple.com/covid19-mobility-data/current/v2/index.json)
 url=$( echo https://covid19-static.cdn-apple.com`jq -n "$x" |jq -r '.basePath'``jq -n "$x"|jq -r '.regions."en-us".csvPath'
 `)
 curl -s $url -o /tmp/latestAppleCovidData.csv
