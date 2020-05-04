e7db42ff082318db546858517e2b5647562acd33
Felipe Hoffa <fhoffa@users.noreply.github.com>
2020-05-04 15:43:33 -0700

    progressive moving avg to exact numbers

--- covid-19-queries/apple_mobility/main.sql
+++ covid-19-queries/apple_mobility/main.sql
@@ -12,8 +12,27 @@ WITH data AS (
   WHERE a._2020_04_27 IS NOT NULL
 ), annotated_data AS (
   SELECT *
-    , -1+EXP(AVG(LOG(1+percent)) OVER(PARTITION BY geo_type, region, transportation_type ORDER BY date DESC
-      rows between 6 preceding and current row)) avg7day 
+    , -1+
+    CASE 
+      WHEN DATE_DIFF(CURRENT_DATE(), date,  DAY) < 4
+      THEN EXP(AVG(LOG(1+percent)) OVER(PARTITION BY geo_type, region, transportation_type ORDER BY date DESC
+        rows between 0 preceding and current row)) 
+      WHEN DATE_DIFF(CURRENT_DATE(), date,  DAY) < 11
+      THEN EXP(AVG(LOG(1+percent)) OVER(PARTITION BY geo_type, region, transportation_type ORDER BY date DESC
+        rows between 2 preceding and current row)) 
+      WHEN DATE_DIFF(CURRENT_DATE(), date,  DAY) < 18
+      THEN EXP(AVG(LOG(1+percent)) OVER(PARTITION BY geo_type, region, transportation_type ORDER BY date DESC
+        rows between 3 preceding and current row)) 
+      WHEN DATE_DIFF(CURRENT_DATE(), date,  DAY) < 25
+      THEN EXP(AVG(LOG(1+percent)) OVER(PARTITION BY geo_type, region, transportation_type ORDER BY date DESC
+        rows between 4 preceding and current row)) 
+      WHEN DATE_DIFF(CURRENT_DATE(), date,  DAY) < 32
+      THEN EXP(AVG(LOG(1+percent)) OVER(PARTITION BY geo_type, region, transportation_type ORDER BY date DESC
+        rows between 5 preceding and current row))       
+      ELSE EXP(AVG(LOG(1+percent)) OVER(PARTITION BY geo_type, region, transportation_type ORDER BY date DESC
+        rows between 6 preceding and current row)) 
+    END avg7day  
+      
     , geo_type||transportation_type||region series_id
   FROM data
 ), lat_lons AS  (
